f6a26c635f8f87715c9a5cd4ae807f3ac38c6517
Blake Lingenau <blake.lingenau@gmail.com>
2015-03-03 16:15:47 -0500

    Added GNU license to existing python programs

--- asker.py
+++ asker.py
@@ -1,3 +1,22 @@
+'''
+Eve Online Market Analysis Tool
+    Copyright (C) 2015  Eve Online Market Analysis Tool Team
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+
 import search
 
 s = raw_input('What would you like to do?\n1. Request name\n2. Buy Information\n3. Sell Information\n')
