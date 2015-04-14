6f675ad35d78b926fe1c23357206289c57fe1a8f
Mike Macelletti <mikem@realityi.com>
2015-04-14 16:42:31 -0400

    Added P/E ratio calculator

--- functions.cpp
+++ functions.cpp
@@ -80,3 +80,20 @@ vector<int> HistoricalHighs(vector<float> Prices)
 	}
 	return Answer;
 }
+
+// Returns the P/E ratio of an item over the last 12 months
+// High P/E ratios generally imply riskier investments
+// If the stock has dropped in the past 12 months, there is no P/E ratio
+float peRatio(vector<float> prices)
+{
+	// Assumes prices is vector of all prices over the past 12 months
+	// Assumes prices[0] is price from 12 months before prices[prices.size() - 1]
+	float PE = -1;
+	
+	if (prices[prices.size() - 1] - prices[0] <= 0)
+	{
+		return PE;
+	}
+	PE = prices[prices.size()-1] / (prices[prices.size() - 1] - prices[0]);
+	return PE;
+}
