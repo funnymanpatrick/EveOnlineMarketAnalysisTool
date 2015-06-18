451efaa46a9a889f015ccdbc0f982906a42cea80
Barry Hu <hub3@rpi.edu>
2015-06-18 16:13:26 -0400

    Orders now bar graph and some clean up

--- dyGraph/graph.js
+++ dyGraph/graph.js
@@ -2,57 +2,64 @@ g = new Dygraph(
     // containing div
     document.getElementById("graphdiv"),
     //CSV file path
-    "example.csv",
+    "item18.csv",
     {
-        //Optional formatting things
-        title:"Item 34, Region 10000002, 5/10-6/07",
+        //Formatting options
+        title:"Relevant Title",
         xlabel:"Date",
         ylabel:"Price",
-        y2label:"",
-        visibility:[true,true,true,false,false],
-        orders:{axis:{ }},
-        volume:{axis:"orders",plotter:barChartPlotter}
+        y2label:"Volume",
+        visibility:[true,true,true,false,true],
+        orders:{axis:{ },plotter:barChartPlotter,highlightCircleSize:0},
+        volume:{axis:"orders",plotter:barChartPlotter,highlightCircleSize:0},
+        axes:{
+            y:{
+                valueFormatter:function(y){return y.toFixed(2);}
+            },
+            y2:{
+                valueFormatter:function(y2){return y2.toFixed(0);},
+                axisLabelFormatter:function(y2){return y2.toFixed(1);}
+            }
+        },
+        dateWindow:[1431975600000,1434412800000]
     }
-
 );
 setStatus();
 
 //Sets and updates status
 function setStatus() {
-    document.getElementById("visibility").innerHTML =
-    	g.visibility().toString();
+    document.getElementById("visibility").innerHTML = g.visibility().toString();
 }
+
 //Sets visibility of graphs based on checkbox status
 function change(el) {
     //Sets visibility of element
     g.setVisibility(parseInt(el.id), el.checked);
 
-    //Only shows orders and defaults back to prices when unchecked
+    //Shows Orders
     if(parseInt(el.id)==3){
-        if(document.getElementById("4").checked){
-            document.getElementById("4").checked = false;
-            g.setVisibility(4,false);
-        }
         if(el.checked){
             g.updateOptions({y2label:"Orders"});
+            if(document.getElementById("4").checked)
+            {
+                document.getElementById("4").checked = false;
+                g.setVisibility("4",false);
+            }
         }
         else{
             g.updateOptions({y2label:""});
         }
     }
 
-    //Only shows volume and defaults back to prices when unchecked
+    //Shows Volume
     if(parseInt(el.id) == 4){
-        if(document.getElementById("3").checked){
-            document.getElementById("3").checked = false;
-            g.setVisibility(3,false);
-        }
         if(el.checked){
-            g.updateOptions({y2label:"Volume",
-                axes:{y2:{
-                    valueFormatter:function(y2,opts){
-                        return y2*.0000000001;
-                    }}}});
+            g.updateOptions({y2label:"Volume"});
+            if(document.getElementById("3").checked)
+            {
+                document.getElementById("3").checked = false;
+                g.setVisibility("3",false);
+            }
         }
         else{
             g.updateOptions({y2label:""});
@@ -60,33 +67,37 @@ function change(el) {
     }
     setStatus();
 }
+
 //Toggles price checkboxes to given state(bool)
-function togglePrices(state){
+//Currently obsolete
+/*function togglePrices(state){
     checkboxes = document.getElementsByName("Price");
     for(i = 0, n=checkboxes.length;i<n;i++){
         checkboxes[i].checked = state;
         g.setVisibility(i,checkboxes[i].checked);
     }
     setStatus();
-}
+}*/
 
 function barChartPlotter(e) {
   var ctx = e.drawingContext;
   var points = e.points;
   var y_bottom = e.dygraph.toDomYCoord(0);  // see http://dygraphs.com/jsdoc/symbols/Dygraph.html#toDomYCoord
- 
+
   // This should really be based on the minimum gap
   var bar_width = 1/2 * (points[1].canvasx - points[0].canvasx);
   ctx.fillStyle = e.color;
- 
+  var area = e.plotArea;
+
   // Do the actual plotting.
   for (var i = 0; i < points.length; i++) {
     var p = points[i];
     var center_x = p.canvasx;  // center of the bar
- 
-    ctx.fillRect(center_x - bar_width / 2, p.canvasy,
-        bar_width, y_bottom - p.canvasy);
-    ctx.strokeRect(center_x - bar_width / 2, p.canvasy,
-        bar_width, y_bottom - p.canvasy);
+    
+    var barHeight = (area.h/p.canvasy)*10;
+    var position = area.h - barHeight + 28;    
+
+    ctx.fillRect(center_x - bar_width / 2, position, bar_width, barHeight);
+    ctx.strokeRect(center_x - bar_width / 2, position, bar_width, barHeight);
   }
 }
--- dyGraph/itemParse.py
+++ dyGraph/itemParse.py
@@ -6,11 +6,11 @@ from pprint import pprint
 
 #http://api.eve-marketdata.com/api/item_history2.json?char_name=demo&region_ids=10000002&type_ids=34,456
 #Add feasible item ID's to end of url
-with open('item34.json') as data_file:
+with open('item18.json') as data_file:
 	data = json.load(data_file)
 #Dict of relevant data
 results = data['emd']['result']
-csv_data = csv.writer(open('example.csv','wb+'))
+csv_data = csv.writer(open('item18.csv','wb+'))
 #typeID,regionID,date,lowPrice,highPrice,avgPrice,volume,orders
 csv_data.writerow("date,highPrice,lowPrice,avgPrice,orders,volume".split(','))
 for item in results:
